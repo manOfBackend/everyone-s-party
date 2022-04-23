012cea3c24e78dd4769bc823d86c70df29aa359b
hyunbell <man.of.backend@gmail.com>
2022-04-23 21:33:58 +0900

    :tada: :wrench: [#1] chore: webpack 설정 추가

---
+++ config/webpack/webpack.dev.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const baseConfig = require('./webpack.base');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const PROJECT_ROOT = path.resolve(__dirname, '../..');
+const TEMPLATE_PATH = path.resolve(PROJECT_ROOT, 'public');
+
+module.exports = merge(baseConfig, {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  devServer: {
+    hot: true,
+    port: 'auto',
+    host: '127.0.0.1',
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(TEMPLATE_PATH, 'index.html'),
+      minify: {
+        removeComments: true,
+      },
+    }),
+  ],
+});
