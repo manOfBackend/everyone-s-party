106f6755a2c670f0b368e2e3f4a15c3d3b9d62e6
hyunbell <man.of.backend@gmail.com>
2022-04-24 07:02:53 +0900

    :wrench: [#1] chore: eslint rules 추가 (arrow-function)

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,15 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['prettier', 'react', '@typescript-eslint'],
-  rules: {},
+  rules: {
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
