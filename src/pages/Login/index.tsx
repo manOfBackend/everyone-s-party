1a7a9d60d708a0f8acbca27147d65a3a5aacf0a3
Youngwoo Park <36143152+parkyw1206@users.noreply.github.com>
2022-04-25 21:57:47 +0900

    :lipstick: [#8] Style: 로그인 페이지 기본적인 css 추가

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,4 @@
+import './index.css';
 import { Actions, useDispatch } from '@src/Redux';
 import React, { useState } from 'react';
 
@@ -8,12 +9,33 @@ const Login = () => {
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <input type="text" id="id" value={id} onChange={(e) => setId(e.target.value)} />
-      <input type="text" id="pw" value={pw} onChange={(e) => setPW(e.target.value)} />
-
+    <div className="w-screen h-screen flex items-center justify-center flex-col">
+      <span className="text-xl font-bold">아이디와 비밀번호를 적어주세요.</span>
+      <span className="mt-5">
+        <span className="mr-8">User: </span>
+        <input
+          type="text"
+          id="id"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          className="border border-solid rounded-md border-gray-900 w-48 h-8"
+          placeholder="아이디를 입력해주세요."
+        />
+      </span>
+      <span className="m-5">
+        <span className="mr-1">password:</span>
+        <input
+          type="password"
+          id="pw"
+          value={pw}
+          onChange={(e) => setPW(e.target.value)}
+          className="border border-solid rounded-md border-gray-900 w-48 h-8"
+          placeholder="비밀번호를 입력해주세요."
+        />
+      </span>
       <button
         type="button"
+        className="login-button"
         onClick={() => {
           dispatch(Actions.loginActions.fetchLogin.request({ id, pw }));
         }}
