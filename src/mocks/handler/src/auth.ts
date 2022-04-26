d632cb2cc1331b7ecf6089830cc7887a09a7e2bc
hyunbell <man.of.backend@gmail.com>
2022-04-26 23:34:39 +0900

    [#8] fix: auth mock 로그인 실패해도 200 ok 주도록 변경

--- src/mocks/handler/src/auth.ts
+++ src/mocks/handler/src/auth.ts
@@ -7,28 +7,35 @@ const users: User[] = [
   },
 ];
 
-const login = rest.post<LoginRequestBody, PathParams, LoginResponseBody>('/login', (req, res, ctx) => {
-  const { id, pw } = req.body;
+const login = rest.post<LoginRequestBody, PathParams, LoginResponseBody>(
+  '/login',
+  (req, res, ctx) => {
+    const { id, pw } = req.body;
 
-  if (users.find((user) => user.id === id && user.pw === pw)) {
+    let message = '';
+    let isLogin = false;
+    const registeredUser = users.find((user) => user.id === id);
+
+    if (registeredUser === undefined) {
+      message = '존재하지 않는 아이디입니다.';
+    }
+
+    if (registeredUser && registeredUser.pw === pw) {
+      isLogin = true;
+      message = '로그인 성공';
+    } else {
+      message = '비밀번호가 일치하지 않습니다.';
+    }
     return res(
       ctx.status(200),
       ctx.json({
         status: 200,
-        message: 'login success',
-        isLogin: true,
+        message,
+        isLogin,
       }),
     );
-  }
-  return res(
-    ctx.status(500),
-    ctx.json({
-      status: 500,
-      message: '로그인에 실패했습니다.',
-      isLogin: false,
-    }),
-  );
-});
+  },
+);
 
 export default {
   login,
