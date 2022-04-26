f812718198e04e26cd5c9054b3dd4616b3ffcd1f
hyunbell <man.of.backend@gmail.com>
2022-04-26 12:03:55 +0900

    [#4] feat: PostList molecule 추가

---
+++ src/components/molecules/PostList/index.tsx
@@ -0,0 +1,26 @@
+import Post from '@src/components/atoms/Post';
+import React from 'react';
+
+interface PostListProps {
+  posts: Post[];
+}
+
+const PostList = ({ posts }: PostListProps) => (
+  <div className="block">
+    <div className="relative mt-0 py-5 px-4 rounded-lg">
+      <ul className="-my-2">
+        {posts?.map((post) => (
+          <Post
+            key={post.newsId}
+            imageUrl={post.imageUrl}
+            title={post.title}
+            summary={post.summary}
+            regDt={post.regDt}
+          />
+        ))}
+      </ul>
+    </div>
+  </div>
+);
+
+export default PostList;
--- src/pages/Board/index.tsx
+++ src/pages/Board/index.tsx
@@ -1,10 +1,11 @@
 import Post from '@src/components/atoms/Post';
+import PostList from '@src/components/molecules/PostList';
 import { useDispatch, useSelector } from '@src/Redux';
 import React, { useEffect } from 'react';
 import Actions from '../../Redux/src/actions';
 
 const Board = () => {
-  const posts = useSelector((state) => state.postReducer.posts);
+  const posts = useSelector((state) => state.postReducer.posts) || [];
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -12,23 +13,9 @@ const Board = () => {
   }, [dispatch]);
 
   return (
-    <div className="block">
-      <div className="relative mt-0 py-5 px-4 rounded-lg">
-        <ul className="-my-2">
-          {
-            posts?.map((post) => (
-              <Post
-                key={post.newsId}
-                imageUrl={post.imageUrl}
-                title={post.title}
-                summary={post.summary}
-                regDt={post.regDt}
-              />
-            ))
-          }
-        </ul>
-      </div>
-    </div>
+    <PostList
+      posts={posts}
+    />
   );
 };
 
