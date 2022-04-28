1168b6ab492715a144f05df0967d7af4521977e6
parkyw1206 <36143152+parkyw1206@users.noreply.github.com>
2022-04-28 12:51:45 +0900

    [#8] fix:  SchemaOf를 이용하여 type이 작동되도록 수정

--- src/Redux/src/sagas/src/login.saga.ts
+++ src/Redux/src/sagas/src/login.saga.ts
@@ -2,17 +2,19 @@
 import API from '@src/services/api';
 import { call, fork, put, takeLatest } from 'redux-saga/effects';
 import * as yup from 'yup';
-// import { SchemaOf, string } from 'yup';
+import { SchemaOf } from 'yup';
 import Actions from '../../actions';
 
 function* fetchLoginSaga({ payload }: { payload: LoginRequestBody }) {
   const { fetchLogin } = Actions.loginActions;
-  const schema = yup.object().shape({
-    id: yup.string()
+  const schema: SchemaOf<LoginRequestBody> = yup.object().shape({
+    id: yup
+      .string()
       .min(5, '아이디는 최소 5글자 이상입니다.')
       .max(16, '아이디는 최대 16글자 입니다.')
       .required('아이디를 입력하세요.'),
-    pw: yup.string()
+    pw: yup
+      .string()
       .min(4, '비밀번호는 최소 4글자 이상입니다.')
       .max(16, '비밀번호는 최대 16글자 입니다.')
       .required('비밀번호를 입력하세요.'),
