9f7feba29776dec4c25d98a1053417661597a106
Youngwoo Park <36143152+parkyw1206@users.noreply.github.com>
2022-05-02 22:36:48 +0900

    :lipstick: [#8] Style: 로그인 페이지를 확정 디자인으로 변경

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { Actions, useDispatch } from '@src/Redux';
 import React, { useState } from 'react';
 import './index.css';
@@ -7,41 +9,55 @@ const Login = () => {
 
   const [id, setId] = useState('');
   const [pw, setPW] = useState('');
-
+  
   return (
-    <div className="w-screen h-screen flex items-center justify-center flex-col">
-      <span className="text-xl font-bold">아이디와 비밀번호를 적어주세요.</span>
-      <span className="mt-5">
-        <span className="mr-8">User: </span>
-        <input
-          type="text"
-          id="id"
-          value={id}
-          onChange={(e) => setId(e.target.value)}
-          className="border border-solid rounded-md border-gray-900 w-48 h-8"
-          placeholder="아이디를 입력해주세요."
-        />
-      </span>
-      <span className="m-5">
-        <span className="mr-1">password:</span>
-        <input
-          type="password"
-          id="pw"
-          value={pw}
-          onChange={(e) => setPW(e.target.value)}
-          className="border border-solid rounded-md border-gray-900 w-48 h-8"
-          placeholder="비밀번호를 입력해주세요."
-        />
-      </span>
-      <button
-        type="button"
-        className="login-button"
-        onClick={() => {
-          dispatch(Actions.loginActions.fetchLogin.request({ id, pw }));
-        }}
-      >
-        로그인
-      </button>
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
+      <div className="p-8 w-full max-w-2xl">
+        <div className="header">
+          <button type="button" className="backbtn">
+            뒤
+          </button>
+        </div>
+        <div className="mt-10 h-4/5">
+          <span className="text-xl font-bold">아이디와 비밀번호를 적어주세요.</span>
+          <div className="mt-5">
+            <span className="mr-8">아이디</span>
+            <input
+              type="text"
+              id="id"
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+              className="inputField"
+              placeholder="아이디를 입력해주세요."
+            />
+          </div>
+          <div className="mt-5">
+            <span className="mr-1">비밀번호</span>
+            <input
+              type="password"
+              id="pw"
+              value={pw}
+              onChange={(e) => setPW(e.target.value)}
+              className="inputField"
+              placeholder="비밀번호를 입력해주세요."
+            />
+          </div>
+          <div
+            className={id.length < 3 ? 'login-button' : 'login-button--dis'}
+            onClick={() => (id.length >= 3 ? dispatch(Actions.loginActions.fetchLogin.request({ id, pw })) : '')}
+          >
+            로그인
+          </div>
+          <div
+            className="kakao-button"
+            onClick={() => {
+              dispatch(Actions.loginActions.fetchLogin.request({ id, pw }));
+            }}
+          >
+            카카오로 시작하기
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
