5e217c75b289849c5a3d18afaf6ccdfb68f64189
Youngwoo Park <36143152+parkyw1206@users.noreply.github.com>
2022-05-12 00:01:54 +0900

    [#21] feat: 다른 function을 사용하지 않고 formik을 사용하여 데이터 관리를 보기 쉽게 설정

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,38 +1,40 @@
+/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 // import { Actions, useDispatch } from '@src/Redux';
 import React, { useState } from 'react';
 import { Actions, useDispatch } from '@src/Redux';
+import { Form, Formik, FormikProps } from 'formik';
+import * as Yup from 'yup';
+import { TextField } from '@material-ui/core';
+
+interface SignupFormValue {
+  id: string;
+  pw: string;
+  pw2: string;
+  name: string;
+}
 
 const Signup = () => {
   const dispatch = useDispatch();
 
-  const [userInfo, setUserInfo] = useState({
-    id: '',
-    pw: '',
-    pw2: '',
-    name: '',
+  const validationSchema = () => Yup.object().shape({
+    id: Yup.string()
+      .min(4, ({ min }) => `비밀번호는 최소 ${min}글자 이상입니다.`)
+      .max(16, ({ max }) => `비밀번호는 최소 ${max}글자 이상입니다.`)
+      .required('아이디를 입력해주세요.'),
+    pw: Yup.string()
+      .min(4, ({ min }) => `비밀번호는 최소 ${min}글자 이상입니다.`)
+      .max(16, ({ max }) => `비밀번호는 최소 ${max}글자 이상입니다.`)
+      .required('비밀번호를 입력하세요.'),
+    pw2: Yup.string()
+      .required('비밀번호를 입력하세요.'),
+    name: Yup.string()
+      .min(1, ({ min }) => `이름은 최소 ${min}글자 이상입니다.`)
+      .max(10, ({ max }) => `이름은 최소 ${max}글자 이상입니다.`)
+      .matches(/(([A-Za-z])\w+)/),
   });
-  const [error, setError] = useState({
-    errorType: '',
-    errorMsg: '',
-  });
-  const checkIdAndPWRegex = () => {
-    const isValid = userInfo.id.length >= 5
-      && userInfo.id.length <= 16
-      && userInfo.pw.length >= 4
-      && userInfo.pw.length <= 16;
-    return isValid;
-  };
-  const submitNewInfo = () => {
-    if (userInfo.pw !== userInfo.pw2) {
-      setError({ errorType: 'pw2', errorMsg: '비밀번호와 다릅니다. 다시 입력해주세요.' });
-      return;
-    }
-    setError({ errorType: '', errorMsg: '' });
-    // eslint-disable-next-line max-len
-    dispatch(Actions.signupActions.fetchSignup.request({ id: userInfo.id, pw: userInfo.pw, name: userInfo.name }));
-  };
+
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center">
       <div className="p-8 w-full max-w-2xl">
@@ -43,64 +45,89 @@ const Signup = () => {
         </div>
         <div className="mt-10 h-4/5">
           <span className="text-xl font-bold">회원 정보를 입력해주세요.</span>
-          <div className="mt-5">
-            <span className="mr-8">아이디</span>
-            <input
-              type="text"
-              id="id"
-              value={userInfo.id}
-              onChange={(e) => setUserInfo({ ...userInfo, id: e.target.value })}
-              className="inputField"
-              placeholder="아이디를 입력해주세요."
-            />
-          </div>
-          <div className="mt-5">
-            <span className="mr-8">이름</span>
-            <input
-              type="text"
-              id="name"
-              value={userInfo.name}
-              onChange={(e) => setUserInfo({ ...userInfo, name: e.target.value })}
-              className="inputField"
-              placeholder="이름 입력해주세요."
-            />
-          </div>
-          <div className="mt-5">
-            <span className="mr-1">비밀번호</span>
-            <input
-              type="password"
-              id="pw"
-              value={userInfo.pw}
-              onChange={(e) => setUserInfo({ ...userInfo, pw: e.target.value })}
-              className="inputField"
-              placeholder="비밀번호를 입력해주세요."
-            />
-          </div>
-          <div className="mt-5">
-            <span className="mr-1">비밀번호 재 입력</span>
-            <input
-              type="password"
-              id="pw2"
-              value={userInfo.pw2}
-              onChange={(e) => setUserInfo({ ...userInfo, pw2: e.target.value })}
-              className="inputField"
-              placeholder="비밀번호를 다시 입력해주세요."
-            />
-            {error && error.errorType === 'pw2' ? (
-              <div className="text-red-500 h-3.5">{error.errorMsg}</div>
-            ) : (
-              <div className="h-3.5" />
-            )}
-          </div>
-          <div
-            className={[
-              'w-full flex items-center h-12 rounded-lg border-none cursor-pointer justify-center mt-40 hover:brightness-95 ',
-              checkIdAndPWRegex() ? 'text-white bg-teal-200' : 'text-neutral-200 bg-neutral-200',
-            ].join()}
-            onClick={() => submitNewInfo()}
+          <Formik
+            initialValues={{
+              id: '',
+              pw: '',
+              pw2: '',
+              name: '',
+            }}
+            validationSchema={validationSchema}
+            onSubmit={(values: SignupFormValue) => {
+              dispatch(Actions.signupActions.fetchSignup.request({ id: values.id, pw: values.pw, name: values.id }));
+            }}
           >
-            확인
-          </div>
+            {({
+              errors,
+              touched,
+              handleSubmit,
+              handleChange,
+              values,
+            }: FormikProps<SignupFormValue>) => (
+              <Form className="flex flex-col gap-4 w-full">
+                <div className="mt-5">
+                  <span className="mr-8">아이디</span>
+                  <TextField
+                    name="id"
+                    className="inputField"
+                    placeholder="아이디를 입력해주세요."
+                    onChange={handleChange}
+                    value={values.id}
+                  />
+                </div>
+                {touched.id && errors.id && (
+                  <div className="text-red-400">{errors.id}</div>
+                )}
+                <div className="mt-5">
+                  <span className="mr-8">이름</span>
+                  <TextField
+                    name="name"
+                    value={values.name}
+                    onChange={handleChange}
+                    className="inputField"
+                    placeholder="이름 입력해주세요."
+                  />
+                </div>
+                {touched.name && errors.name && (
+                  <div className="text-red-400">{errors.name}</div>
+                )}
+                <div className="mt-5">
+                  <span className="mr-1">비밀번호</span>
+                  <TextField
+                    name="pw"
+                    value={values.pw}
+                    onChange={handleChange}
+                    className="inputField"
+                    placeholder="비밀번호를 입력해주세요."
+                  />
+                </div>
+                {touched.pw && errors.pw && (
+                  <div className="text-red-400">{errors.pw}</div>
+                )}
+                <div className="mt-5">
+                  <span className="mr-1">비밀번호 재 입력</span>
+                  <TextField
+                    name="pw2"
+                    value={values.pw2}
+                    onChange={handleChange}
+                    className="inputField"
+                    placeholder="비밀번호를 다시 입력해주세요."
+                  />
+                  {touched.pw2 && errors.pw2 && (
+                  <div className="text-red-400">{errors.pw2}</div>
+                  )}
+                </div>
+                <button
+                  className="w-full flex items-center h-12 rounded-lg border-none cursor-pointer justify-center mt-40 hover:brightness-95 text-white bg-teal-200"
+                  type="submit"
+                  onSubmit={() => handleSubmit()}
+                >
+                  확인
+                </button>
+              </Form>
+            )}
+          </Formik>
+
         </div>
       </div>
     </div>
@@ -108,3 +135,6 @@ const Signup = () => {
 };
 
 export default Signup;
+function id(id: any) {
+  throw new Error('Function not implemented.');
+}
