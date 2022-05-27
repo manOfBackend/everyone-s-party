817130fc36c6a390758f26d96fe52db3b6a29675
hyunbell <man.of.backend@gmail.com>
2022-05-27 16:53:57 +0900

    [#34] style: tailwind 공통 테마 컬러 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,15 @@ module.exports = {
       },
       maxWidth: {
         '4/5': '80%',
-      }
+      },
+      colors: {
+        gray: {
+          gray100: '#F1F3F7',
+          gray600: '#646F7C',
+          gray800: '#282B30',
+        },
+        primary500: '#53E3AC',
+      },
     },
   },
   plugins: [],
