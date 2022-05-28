d4f57c69ee9f1691f40abad824e20daba017da14
Youngwoo Park <36143152+parkyw1206@users.noreply.github.com>
2022-05-28 15:21:33 +0900

    [#33] feat: 파티가 없을 경우, 빈 화면 추가

---
+++ src/pages/MyParty/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import EmptyImage from '@src/assets/resource/empty_addParty.svg';
+import ButtonCTA from '@src/components/atoms/ButtonCTA';
+
+const MyParty = () => (
+  <div className="mt-10">
+    <div className="text-xl font-bold">파티를 추가하고</div>
+    <div className="text-xl font-bold">OTT를 저렴하게 감상하세요.</div>
+    <div className="text-sm text-gray-400 mt-4">
+      커피 한잔값으로 즐기는 컨텐츠의 자유!
+    </div>
+    <div className="flex mt-10 place-content-center">
+      <EmptyImage />
+    </div>
+    <div className="mt-40">
+      <ButtonCTA btnColor="#4DCA9A" textColor="white" onClick={() => yield put(push('/login'))}>
+        서비스 시작하기
+      </ButtonCTA>
+    </div>
+  </div>
+);
+
+export default MyParty;
